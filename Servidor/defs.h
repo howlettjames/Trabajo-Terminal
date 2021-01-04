eaf46aa9fcc5b409f486dafa05302d7c0d3b5532
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-01-03 19:15:17 -0600

    Se agregaron las constantes de Width, Height, Channels y Image Size

--- Servidor/defs.h
+++ Servidor/defs.h
@@ -9,6 +9,11 @@
 #define GAUSS_MASK      3       // Dimensión del filtro(matriz) de Gauss, en este caso de 3x3
 #define SOBEL_MASK      3       // Dimensión del filtro(matriz) de Sobel, en este caso de 3x3
 #define NUM_THREADS     4  		// Número de hilos para procesar la imagen cuando se aplica Sobel
+#define WIDTH_IMG		640
+#define HEIGHT_IMG		480
+#define CHANNELS_IMG	3
+#define IMG_SIZE	    WIDTH_IMG*HEIGHT_IMG*CHANNELS_IMG	// Tamaño de la imagen
+
 
 #endif
 
