010ae5cc743c99d201828d33bcb8af03fa8e9163
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-05-05 22:02:00 -0500

    Utileria que contiene las funciones de procesamiento de imagen

--- utileria.py
+++ utileria.py
@@ -48,25 +48,6 @@ def getHistogram(img, minPer=0.1, display= False, region=1):
     
     return basePoint
 
-# Esto lo hace para toda la imagen
-def getHistogramV1(img, minPer=0.1, display= False):
-    
-    histValues = np.sum(img,axis=0)
-    
-    maxValue = np.max(histValues)
-    minValue = minPer*maxValue
-    
-    indexArray = np.where(histValues >= minValue)
-    basePoint = int(np.average(indexArray))
-    
-    if display:
-        imgHist = np.zeros((img.shape[0],img.shape[1],3), np.uint8)
-        for x, intensity in enumerate(histValues):
-            cv2.line(imgHist, (x,img.shape[0]), (x, img.shape[0]-intensity//255), (255,0,255), 1)
-            cv2.circle(imgHist, (basePoint, img.shape[0]), 20, (0, 255, 255), cv2.FILLED)
-        return basePoint, imgHist
-    
-    return basePoint
     
 def stackImages(scale,imgArray):
     rows = len(imgArray)
