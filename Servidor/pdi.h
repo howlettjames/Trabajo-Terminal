1c9ce8f43ee016fdfa7100f8ba43e0c080f15f31
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-05-05 21:30:58 -0500

    Agregacion de funciones utilizadas para PDI

--- Servidor/pdi.h
+++ Servidor/pdi.h
@@ -14,6 +14,8 @@ void ajusteBrilloImagen(unsigned char * imagenGray, uint32_t width, uint32_t hei
 void GrayToRGB(unsigned char *imagenRGB, unsigned char *imagenGray, uint32_t width, uint32_t height);
 void smoothGauss(unsigned char *imagenGray, unsigned char *imagenGauss, uint32_t width, uint32_t height);
 void *edgeDetection(void *nh);
+void segmentar(unsigned char *imagenGray, unsigned char *imagenSegmentada, uint32_t _width, uint32_t _height, int threshold);
+void sumar(unsigned char *imagenSobel, unsigned char *imagenSegmentada, unsigned char *imagenUnion, uint32_t _width, uint32_t _height);
 unsigned char * reservarMemoria(uint32_t width, uint32_t height);
 
 #endif
